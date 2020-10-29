87c47803d262c4e57c914162421d85bdd699626b
johnathancampana <johnathancampana@Johnathans-Air>
2020-10-29 14:57:32 -0400

    added ranges to training function and created prediction function

--- helpers/stats_helper.py
+++ helpers/stats_helper.py
@@ -1,20 +1,22 @@
 import pandas as pd
 import numpy as np
+import sys
 from helpers.data_helpers import *
 
-def create_word_occurence_like_dict(df_of_tweets): #list of tweets is a list of tweet tuples (content, # of likes)
+def create_word_occurence_like_dict(df_of_tweets, start, end): #list of tweets is a list of tweet tuples (content, # of likes)
     "Given a list of tweets where the second element is a dataframe, returns a dict with the total occurences and total number of likes for each word"
     wordDict = {} #each key is a word that corresponds to (number of occurences of word, number of likes total in tweets containing word)
     for index, tweet in df_of_tweets[0].iterrows():
-        text = tweet[0].lower()
-        like_count = tweet[1]
-        if not np.isnan(like_count):
-            for word in text.split():
-                if word in wordDict:
-                    wordDict[word][0] = wordDict[word][0] + 1
-                    wordDict[word][1] = wordDict[word][1] + like_count
-                else:
-                    wordDict[word] = [1, like_count]
+        if index >= start and index <= end:
+            text = tweet[0].lower()
+            like_count = tweet[1]
+            if not np.isnan(like_count):
+                for word in text.split():
+                    if word in wordDict:
+                        wordDict[word][0] = wordDict[word][0] + 1
+                        wordDict[word][1] = wordDict[word][1] + like_count
+                    else:
+                        wordDict[word] = [1, like_count]
     return wordDict
 
 def calc_avg_likes(wordDict):
@@ -34,18 +36,31 @@ def predict_likes(tweet_text, avg_like_dict):
         if word in avg_like_dict:
             total_likes = total_likes + avg_like_dict[word] #sum the avg likes of all the words
             words_used = words_used + 1
+    if words_used == 0:
+        return 0
     return total_likes / words_used #return the avg likes of a word averaged over all the words in the tweet
+
+def predict_and_compare_likes(df_of_tweets, avg_like_dict, start, end):
+    text_predicted_actual = []
+    for index, tweet in df_of_tweets[0].iterrows():
+        if index >= start and index <= end:
+            text = tweet[0].lower()
+            like_count = tweet[1]
+            if not np.isnan(like_count):
+                predicted = predict_likes(text, avg_like_dict)
+                text_predicted_actual.append([text, predicted, like_count])
+    return text_predicted_actual
 
 parsed_df = get_simple_parsed_dataframe("trump")
 
-wordDict = create_word_occurence_like_dict(parsed_df)
+wordDict = create_word_occurence_like_dict(parsed_df, 0, sys.maxsize)
 
 avg_like_dict = calc_avg_likes(wordDict)
 
 tweet = "this is going to be a really bad bad bad ending"
 
-print("\nexamples/tests")
-#print("\nwordDict: ", wordDict)
-print("\navg_like_dict: ", avg_like_dict)
-print("\npredicted likes in tweet: ", tweet, predict_likes(tweet, avg_like_dict))
+# print("\nexamples/tests")
+# print("\nwordDict: ", wordDict)
+# print("\navg_like_dict: ", avg_like_dict)
+# print("\npredicted likes in tweet: ", tweet, predict_likes(tweet, avg_like_dict))
 
--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from helpers.stats_helper import *
 
 if __name__ == "__main__":
     # example
-    d = parse_raw_data_csv("russian")
-    parsed_df = get_simple_parsed_dataframe("russian")
+    d = parse_raw_data_csv("trump")
+    parsed_df = get_simple_parsed_dataframe("trump")
+    wordDict = create_word_occurence_like_dict(parsed_df, 0, 25000)
+    
+    avg_like_dict = calc_avg_likes(wordDict)
+
+    prediction = predict_and_compare_likes(parsed_df, avg_like_dict, 25000, 50000)
+
+    print(prediction)
+
     pass
