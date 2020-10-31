9f12f6f617f180d178e08a82476d2d1f0811054b
Jeremy <misterj.mackey@gmail.com>
2020-10-31 00:05:11 -0400

    Removed folder from repo

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     d = parse_raw_data_csv("trump")
     parsed_df = get_simple_parsed_dataframe("trump")
     wordDict = create_word_occurence_like_dict(parsed_df, 0, 25000)
-    
+
     avg_like_dict = calc_avg_likes(wordDict)
 
     prediction = predict_and_compare_likes(parsed_df, avg_like_dict, 25000, 50000)
