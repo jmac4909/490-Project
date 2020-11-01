8b2eff21eff4246a8351da1d45b2c54a4d70b1c5
Jeremy <misterj.mackey@gmail.com>
2020-11-01 17:40:43 -0500

    Adding data visuals

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from helpers.stats_helper import *
+import pickle
 
-if __name__ == "__main__":
+
+def run_prediction_and_pickle(data_string):
     # example
-    parsed_df = get_simple_parsed_dataframe("trump")
+    parsed_df = get_simple_parsed_dataframe(data_string)
 
     wordDict = create_word_occurrence_like_dict(parsed_df, 0, 25000)
 
@@ -10,7 +12,13 @@ if __name__ == "__main__":
 
     prediction = predict_and_compare_likes(parsed_df, avg_like_dict, 25000,
                                            50000)
+    with open('notebooks/'+data_string + '.pkl', 'wb') as f:
+        pickle.dump(prediction, f)
 
-    print(prediction)
 
-    pass
+if __name__ == "__main__":
+    run_prediction_and_pickle('trump')
+    run_prediction_and_pickle('avengers')
+    run_prediction_and_pickle('russia')
+    run_prediction_and_pickle('top20')
+    print('done :)')
